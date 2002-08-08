dc3b93222aeb25085005075f1e527dcdea44f97e
Lincoln Stein <lincoln.stein@gmail.com>
2002-08-08 12:13:49 +0000

    language support is functional but not fully tested

--- conf/languages/POSIX.pm
+++ conf/languages/POSIX.pm
@@ -1,21 +1,16 @@
 # do not remove the { } from the top and bottom of this page!!!
 {
-
    #----------
    # MAIN PAGE
    #----------
 
-   SHOWING_FROM_TO => 'Showing %s Kbp from %s, positions %s to %s',
+   PAGE_TITLE => 'Genome browser',
 
    SEARCH_INSTRUCTIONS => <<END,
 Search using a sequence name, gene name,
-locus, oligonucleotide (15 bp minimum) or other landmark. The wildcard
+locus%s, or other landmark. The wildcard
 character * is allowed.
 END
-
-   EXAMPLES => 'Examples',
-
-   HELP     => 'Help',
 
    NAVIGATION_INSTRUCTIONS => <<END,
 To center on a location, click the ruler. Use the Scroll/Zoom buttons
@@ -23,21 +18,62 @@ to change magnification and position. To save this view,
 <a href="%s">bookmark this link.</a>
 END
 
+   EDIT_INSTRUCTIONS => <<END,
+Edit your uploaded annotation data here.
+You may use tabs or spaces to separate fields,
+but fields that contain whitespace must be contained in
+double or single quotes.
+END
+
+   SHOWING_FROM_TO => 'Showing %s from %s, positions %s to %s',
+
    LANDMARK => 'Landmark or Region',
 
    GO       => 'Go',
+
+   FIND     => 'Find',
+
+   DUMP     => 'Dump',
+
+   ANNOTATE     => 'Annotate',
 
    SCROLL   => 'Scroll/Zoom',
 
    RESET    => 'Reset',
 
-   DISPLAY_SETTINGS => 'Display Settings:',
+   DOWNLOAD_FILE    => 'Download File',
+
+   DOWNLOAD_DATA    => 'Download Data',
+
+   DOWNLOAD         => 'Download',
+
+   DISPLAY_SETTINGS => 'Display Settings',
 
    TRACKS   => 'Tracks',
 
    EXTERNAL_TRACKS => '(External tracks italicized)',
 
-   IMAGE_WIDTH => 'Image Width:',
+   EXAMPLES => 'Examples',
+
+   HELP     => 'Help',
+
+   HELP_FORMAT => 'Help with File Format',
+
+   CANCEL   => 'Cancel',
+
+   ABOUT    => 'About...',
+
+   REDISPLAY   => 'Redisplay',
+
+   CONFIGURE   => 'Configure...',
+
+   EDIT       => 'Edit File...',
+
+   DELETE     => 'Delete File',
+
+   EDIT_TITLE => 'Enter/Edit Annotation data',
+
+   IMAGE_WIDTH => 'Image Width',
 
    SET_OPTIONS => 'Set Track Options...',
 
@@ -45,9 +81,9 @@ END
 
    DUMPS       => 'Dumps, Searches and other Operations',
 
-   DATA_SOURCE => 'Data Source:',
+   DATA_SOURCE => 'Data Source',
 
-   UPLOAD_TITLE=> 'Upload your own annotations:',
+   UPLOAD_TITLE=> 'Upload your own annotations',
 
    UPLOAD_FILE => 'Upload a file',
 
@@ -63,6 +99,31 @@ END
 
    PRESETS      => '--Choose Preset URL--',
 
+   FILE_INFO    => 'Last modified %s.  Annotated landmarks: $s',
+
+   FOOTER_1     => <<END,
+Note: This page uses cookie to save and restore preference information.
+No information is shared.
+END
+
+   FOOTER_2    => 'Generic genome browser version %s',
+
+   #----------------------
+   # MULTIPLE MATCHES PAGE
+   #----------------------
+
+   HIT_COUNT      => 'The following %d regions match your request.',
+
+   MATCHES_ON_REF => 'Matches on %s',
+
+   SEQUENCE        => 'sequence',
+
+   SCORE           => 'score=%s',
+
+   NOT_APPLICABLE => 'n/a',
+
+   BP             => 'bp',
+
    #--------------
    # SETTINGS PAGE
    #--------------
@@ -75,9 +136,9 @@ END
 
    REFRESH  => 'Refresh',
 
-   CANCEL   => 'Cancel Changes and Return...',
+   CANCEL_RETURN   => 'Cancel Changes and Return...',
 
-   ACCEPT   => 'Accept Changes and Return...',
+   ACCEPT_RETURN   => 'Accept Changes and Return...',
 
    OPTIONS_TITLE => 'Track Options',
 
@@ -105,18 +166,72 @@ END
 
    LIMIT  => 'Limit',
 
+   ADJUST_ORDER => 'Adjust Order',
+
    CHANGE_ORDER => 'Change Track Order',
 
-   AUTO => 'auto',
+   AUTO => 'Auto',
 
-   COMPACT => 'compact',
+   COMPACT => 'Compact',
 
-   EXPAND => 'expand',
+   EXPAND => 'Expand',
 
-   EXPAND_LABEL => 'Expand &amp; Label',
+   EXPAND_LABEL => 'Expand & Label',
 
    HYPEREXPAND => 'Hyperexpand',
 
-   HYPEREXPAND_LABEL =>'Hyperexpand &amp; label',
+   HYPEREXPAND_LABEL =>'Hyperexpand & label',
+
+   NO_LIMIT    => 'No limit',
+
+   #--------------
+   # HELP PAGES
+   #--------------
+
+   CLOSE_WINDOW => 'Close this window',
+
+   TRACK_DESCRIPTIONS => 'Track Descriptions & Citations',
+
+   BUILT_IN           => 'Tracks Built into this Server',
+
+   EXTERNAL           => 'External Annotation Tracks',
+
+   ACTIVATE           => 'Please activate this track in order to view its information.',
+
+   NO_EXTERNAL        => 'No external features loaded.',
+
+   NO_CITATION        => 'No additional information available.',
+
+   #--------------
+   # PLUGIN PAGES
+   #--------------
+
+ ABOUT_PLUGIN  => 'About %s',
+
+ BACK_TO_BROWSER => 'Back to Browser',
+
+ PLUGIN_SEARCH_1   => '%s (via %s search)',
+
+ PLUGIN_SEARCH_2   => '&lt;%s search&gt;',
+
+ CONFIGURE_PLUGIN   => 'Configure',
+
+ BORING_PLUGIN => 'This plugin has no extra configuration settings.',
+
+   #--------------
+   # ERROR MESSAGES
+   #--------------
+
+ NOT_FOUND => 'The landmark named <i>%s</i> is not recognized. See the help pages for suggestions.',
+
+ TOO_BIG   => 'Detailed view is limited to %s bases.  Click in the overview to select a region %s bp wide.',
+
+ PURGED    => "Can't find the file named %s.  Perhaps it has been purged?.",
+
+ NO_LWP    => "This server is not configured to fetch external URLs.",
+
+ FETCH_FAILED  => "Could not fetch %s: %s.",
+
+ TOO_MANY_LANDMARKS => '%d landmarks.  Too many to list.',
 
 };

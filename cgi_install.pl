ec581d50a869acc2415c3da52c8aa96ab5846283
Scott Cain <scott@scottcain.net>
2002-12-23 18:05:17 +0000

    initial import cgi_install.pl

---
+++ cgi_install.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use File::Copy;
+
+my $cgi_target = $ARGV[0];
+
+print "Installing gbrowse CGI script...\n";
+
+if (! (-e $cgi_target) ) {
+    mkdir $cgi_target or die "unable to create $cgi_target directory\n";
+}
+
+copy("gbrowse",    "$cgi_target/gbrowse") or die "unable to copy to $cgi_target/gbrowse\n";
+my $mode = 0755;
+chmod $mode, "$cgi_target/gbrowse" or die "unable to make $cgi_target/gbrowse world executable\n";
+
+copy("gbrowse_img", "$cgi_target/gbrowse_img") or die "unable to copy to $cgi_target/gbrowse_img\n";
+chmod $mode, "$cgi_target/gbrowse_img" or die "unable to make $cgi_target/gbrowse_img world executable\n";

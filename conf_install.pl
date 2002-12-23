f32e39845cb12b6da78b05d4d7f1497e867b5918
Scott Cain <scott@scottcain.net>
2002-12-23 20:29:18 +0000

    path delimiter fixes so will work with Windows

--- conf_install.pl
+++ conf_install.pl
@@ -5,51 +5,62 @@ use warnings;
 #Perform installation of gbrowse apache configuration files
 
 use File::Copy;
+use Config;
 
 # get configuration stuff from command line
-my $CONF = $ARGV[0];
+my $dir = $ARGV[0];
+
+my $delim = '/';
+if ($Config{'osname'} =~ /win/i) {
+    $dir =~ s!\/!\\!g;
+    $delim = '\\';    
+}
 
 #start the installation...
 print "Installing sample configuration files...\n";
-my $dir = "$CONF/gbrowse.conf";
 
 if (! (-e $dir)) {
     mkdir $dir or die "unable to make $dir directory\n";
 }
 
-opendir CONFDIR, "./conf" or die "unable to opendir ./conf\n";
+opendir CONFDIR, "conf" or die "unable to opendir conf\n";
 while (my $conffile = readdir(CONFDIR) ) {
-    if (-f "./conf/$conffile") {
-        if (-f "$dir/$conffile") {
+    if (-f "conf" . $delim . $conffile) {
+        if (-f $dir . $delim . $conffile) {
 	    print "   Found $conffile in $dir. Skipping...\n";
         } else {
-	    copy("./conf/$conffile", "$dir/$conffile") or die "unable to copy to$dir/$conffile\n";
+	    copy("conf" . $delim . $conffile, $dir . $delim . $conffile) 
+                or die "unable to copy to $dir/$conffile\n";
         }
     }
 }
 closedir CONFDIR;
 
-if (! (-e "$dir/plugins")) {
-    mkdir "$dir/plugins" or die "unable to mkdir $dir/plugins\n";
+my $plugindir = $dir . $delim . "plugins";
+if (! (-e $plugindir)) {
+    mkdir $plugindir or die "unable to mkdir $plugindir\n";
 }
 
-opendir PLUGINS, "./conf/plugins" or die "unable to opendir ./conf/plugins\n";
+opendir PLUGINS, "conf/plugins" or die "unable to opendir ./conf/plugins\n";
 while (my $pluginfile = readdir(PLUGINS) ) {
-    if (-f "./conf/plugin/$pluginfile") {
-        copy($pluginfile, "$dir/plugins/$pluginfile") or die "unable to copy to $dir/plugins/$pluginfile\n";
+    if (-f "./conf/plugins/$pluginfile") {
+        copy("conf/plugins" . $delim . $pluginfile, $plugindir . $delim . $pluginfile) 
+            or die "unable to copy to $dir/plugins/$pluginfile\n";
     } 
 }
 closedir PLUGINS;
 
-if (! (-e "$dir/languages")) {
-    mkdir "$dir/languages" or die "unable to mkdir $dir/languages\n";
+my $langdir = $dir . $delim . 'languages';
+if (! (-e $langdir)) {
+    mkdir $langdir or die "unable to mkdir $langdir\n";
 }
 
-opendir LANGDIR, "./conf/languages" or die "unable to opendir ./conf/languages\n";
-while (my $langfile = readdir(LANGDIR)) {
+opendir LANGS, "conf/languages" or die "unable to opendir ./conf/languages\n";
+while (my $langfile = readdir(LANGS)) {
     if (-f "./conf/languages/$langfile") {
-        copy($langfile, "$dir/languages/$langfile") or die "unable to copy to $dir/languages/$langfile\n";
+        copy("conf/languages" . $delim . $langfile, $langdir . $delim . $langfile) 
+            or die "unable to copy to $dir/languages/$langfile\n";
     }
 }
-closedir LANGDIR;
+closedir LANGS;
 

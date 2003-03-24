ef8471b429e9bbc59803a4193a52a9b898a9dad9
Lincoln Stein <lincoln.stein@gmail.com>
2003-03-24 18:32:18 +0000

    fixed stupid bug that prevented the "beneath" option from working

---
+++ lib/Bio/Graphics/Glyph/connector.pm
@@ -0,0 +1,11 @@
+package Bio::Graphics::Glyph::connector;
+
+# just a line, for DAS compatibility
+use strict;
+use Bio::Graphics::Glyph::generic;
+use Bio::Graphics::Glyph::line;
+use vars '@ISA';
+@ISA = 'Bio::Graphics::Glyph::line';
+1;
+
+__END__

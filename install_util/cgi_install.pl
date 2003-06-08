1d3a8f099eba6f8b5e0b81a48b3b7b6a1eaa293a
Lincoln Stein <lincoln.stein@gmail.com>
2003-06-08 18:50:39 +0000

    moved the .PLS files into cgi-bin

--- install_util/cgi_install.pl
+++ install_util/cgi_install.pl
@@ -5,21 +5,20 @@ use Bio::Root::IO;
 
 my $cgi_target = $ARGV[0];
 
-# now using Bio::Root::IO
-#my $delim = '/';
-#if ($Config{'osname'} =~ /win/i && $Config{'osname'} !~ /darwin/i ) {
-#    $cgi_target =~ s!\/!\\!g;
-#    $delim = '\\';
-#}
-
-print "Installing gbrowse CGI script...\n";
+print "Installing gbrowse CGI scripts...\n";
 
 if (! (-e $cgi_target) ) {
     mkdir($cgi_target,0777) or die "unable to create $cgi_target directory\n";
 }
 
-foreach (qw(gbrowse gbrowse_img gbrowse_details)) {
+chdir 'cgi-bin';
+foreach (glob('*')) {
+  next if /README/;
+  next if /CVS/;
+  next if /^[.\#]/;
+  next if /~$/;
   my $installfile = Bio::Root::IO->catfile($cgi_target, $_);
+  warn "copying $_ to $installfile";
   copy($_, $installfile ) or die "unable to copy to $installfile\n";
   my $mode = 0755;
   chmod $mode, $installfile

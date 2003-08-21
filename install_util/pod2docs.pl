f839e696a7e8d914201d030414da012e396b4758
Scott Cain <scott@scottcain.net>
2003-08-21 17:07:32 +0000

    more doc clean up--Note that this version of INSTALL is derived from docs/pod/INSTALL.pod.  As ugly as that is, I prefer the result when it is installed in htdocs.

--- install_util/pod2docs.pl
+++ install_util/pod2docs.pl
@@ -3,27 +3,23 @@ use strict;
 use File::Find;
 use File::Basename;
 
-
 #the point here is to generate text documents from pod
 #it is really a tool for us (Lincoln and Scott) for generating
 #text documentation that will go into a software release
 
-#the appending of '.txt' to the file name is temporary for testing,
-#after this method of generating text documentation is finalized,
-#that will be changed to strip '.pod' from the text file name.
-
 find({wanted => \&wanted, no_chdir=>1}, '.');
-
-unlink './pod2htmd.x~~';
-unlink './pod2htmi.x~~';
 
 sub wanted {
   if (/\.pod$/) {
     if ('INSTALL.pod' eq basename($File::Find::name)) {
-      system('pod2text','-l',$File::Find::name,'INSTALL.txt');
+      system('pod2text','-l',$File::Find::name,'INSTALL');
     } else {
-      my $basename = basename($File::Find::name);
-      system('pod2text','-l',$File::Find::name,"./docs/$basename.txt"); 
+      my ($basename,undef,undef) = fileparse($File::Find::name,"\.pod");
+      if (uc($basename) eq $basename) {
+        system('pod2text','-l',$File::Find::name,"./docs/$basename");
+      } else {
+        system('pod2text','-l',$File::Find::name,"./docs/$basename.txt"); 
+      }
     }
   }
 }

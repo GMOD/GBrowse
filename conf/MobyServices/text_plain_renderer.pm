2f1fd8f2cc157da85c1d8bece44de92440d0738c
Mark Wilkinson <markwilkinson@users.sourceforge.net>
2004-01-02 17:16:17 +0000

    after a rethink about rendering, the renderer is now able to tell Gbrowse if it has or has not been able to deal with the entirety of the MOBY Object it was given, and if not, Gbrowse will begin unpacking the object and rendering its subcomponents.

--- conf/MobyServices/text_plain_renderer.pm
+++ conf/MobyServices/text_plain_renderer.pm
@@ -1,5 +1,6 @@
 package MobyServices::text_plain_renderer;
 use strict;
+use XML::DOM;
 our @ISA = qw(Exporter);
 #our @EXPORT = qw(render type);
 our @EXPORT_OK = qw(render type);
@@ -10,17 +11,24 @@ sub type {
 }
 
 sub render {
-    my ($data, $htmldir,$imgdir) = @_;
-    return "$data";
+    my ($DOM, $htmldir,$imgdir) = @_;
+    my $content;
+    foreach my $subnode($DOM->getChildNodes){
+        next unless  (($subnode->getNodeType == TEXT_NODE) || ($subnode->getNodeType == CDATA_SECTION_NODE));
+        $content .=$subnode->toString;
+    }
+
+    return ("$content",0);# the 0 indicates that we have only rendered the top-level XML of this object
 }
 
 1;
 
 
 
+
 =head1 NAME
 
-text_plain_renderer.pm - a renderer (HTML) for text-plain type MOBY Objects
+text_plain_renderer.pm - a renderer (HTML) for text_plain type MOBY Objects
 
 =head1 AUTHOR
 
@@ -34,8 +42,10 @@ and it will work.
 =head1 DESCRIPTION
 
 This renderer returns HTML that fits between the
-<td>; tags in a web-page to display the content
-of the text-plain (or ontological child of) object.
+<td> tags in a web-page to display the content
+of a text_plain (or ontological child of) object.
+
+=head1 METHODS
 
 The module has two methods:
 
@@ -43,16 +53,56 @@ The module has two methods:
 
 =item type
 
-this returns a scalar indicating the MOBY Object Ontology
-class that this renderer is designed to handle.  Objects
+this returns a scalar indicating the MOBY Object
+Class that this renderer is designed to handle.  Objects
 of this type, or objects that inherit from this type,
 will be passed to this renderer.
 
 =item render
 
-This accepts the string representing the data contained in the
-object and returns HTML that will represent that object
-properly when put into a table-cell.
+This is called with three pieces of data which may or may not
+be useful to your script:
+    
+=over
+
+=item 1)  $data - an XML::DOM object
+representing the deserialized MOBY object
+
+
+=item 2)  $htmldir - the full path to the directory serving your html
+
+ e.g. /usr/local/apache/htdocs/
+ 
+ (this is the HTDOCS parameter you specified
+  when you installed Gbrowse)
+
+=item 3)  $imgdir - the additional path information to a writable directory for images
+
+ e.g. /gbrowse/tmp
+ 
+ (this is the folder specified in the tmpimages parameter
+  in your organism.conf file)
+
+
+=back
+
+=back
+
+=head1 RETURNS
+
+The subroutine should return two pieces of data:
+
+=over
+
+=item 1)  An HTML representation of the Object
+
+ this will appear between <td></td> tags in the webpage
+
+=item 2)  A boolean indicating whether the renderer has parsed all sub-objects, or just the top level object
+
+ '1' indicates that the renderer has fully parsed the Object XML
+ '0' indicates that you need Gbrowse to follow child objects
+     and render them independently
 
 =back
 

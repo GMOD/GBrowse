c8a9348ddf762e55f1e8a9db36b12a9407b254fc
Scott Cain <scott@scottcain.net>
2004-06-21 13:37:24 +0000

    updating Spanish lang file

--- conf/languages/es.pm
+++ conf/languages/es.pm
@@ -1,4 +1,5 @@
 # do not remove the { } from the top and bottom of this page!!!
+# Author: Marcela Karey Tello-Ruiz <marcela@broad.mit.edu>
 {
 
  CHARSET =>   'ISO-8859-1',
@@ -37,9 +38,70 @@ END
 
    SHOW_INSTRUCTIONS => 'Mostrar Instrucciones',
 
+   HIDE_INSTRUCTIONS => 'Ocultar Instrucciones',
+
+   SHOW_HEADER       => 'Mostrar Encabezado',
+
+   HIDE_HEADER       => 'Ocultar Encabezado',
+
    LANDMARK => 'Punto o Región de Referencia',
 
    BOOKMARK => 'Marcar esta Página',
+
+   IMAGE_LINK => 'Liga a una imagen de esta vista',
+
+   SVG_LINK   => 'Calidad de imagen para publicación',
+
+   SVG_DESCRIPTION => <<END,
+<p>
+La siguiente liga generará esta imagen en un formato de Vector Escalable (SVG).  Imagenes SVG ofrecen varias ventajas sobre las imágenes basadas en raster, tales como los formatos jpeg o png.
+</p>
+<ul>
+<li>totalmente escalable sin pérdida en resolución
+<li>editable aspecto-por-aspecto en aplicaciones gráficas basadas en vectores
+<li>si es necesario, puede ser convertido en EPS para ser incluído en una publicación
+</ul>
+<p>
+Para ver imagenes SVG, necesitas un buscador (browser) que sea capaz de aceptar SVG, el accesorio (plugin) para buscadores Adobe SVG, o una aplicación para ver o editar SVG tal como Adobe Illustrator.
+</p>
+<p>
+Accesorio (plugin) para buscadores Adobe SVG: <a
+href="http://www.adobe.com/support/downloads/product.jsp?product=46&platform=Macintosh">Macintosh</a>
+| <a
+href="http://www.adobe.com/support/downloads/product.jsp?product=46&platform=Windows">Windows</a>
+<br />
+Usuarios de Linux pueden explorar el <a href="http://xml.apache.org/batik/">visualizador Batik SVG</a>.
+</p>
+<p>
+<a href="%s" target="_blank">Ver imagen SVG en una ventana distinta</a></p>
+<p>
+para guardar esta imagen en tu disco, pulsa la tecla de control (Macintosh) o el botón de la derecha de tu ratón (Windows) y selecciona la opción para guardar la liga correspondiente.
+</p>   
+END
+
+   IMAGE_DESCRIPTION => <<END,
+<p>
+Para crear una imagen montada/incrustada de esta vista, corta y pega esta dirección (URL):
+</p>
+<pre>
+&lt;IMAGE src="%s" /&gt;
+</pre>
+<p>
+La imagen se verá asi:
+</p>
+<p>
+<img src="%s" />
+</p>
+
+<p>
+Si sólo aparece la vista global (cromosoma o contig), trata de reducir el tamaño de la región.
+</p>
+END
+
+   TIMEOUT  => <<'END',
+Tu solicitud expiró.  Posiblemente seleccionaste una región muy grande para ver.
+Puedes eliminar algunas de las pistas que seleccionaste antes o intentar ver una región mas pequeña. Si esto te sucede continuamente, por favor presiona el botón rojo que dice "Reiniciar".
+END
 
    GO       => 'Ir',
 
@@ -47,13 +109,17 @@ END
 
    SEARCH   => 'Buscar',
 
-   DUMP     => 'Tirar',
+   DUMP     => 'Depositar',
+
+   HIGHLIGHT   => 'Resaltar',
 
    ANNOTATE     => 'Anotar',
 
    SCROLL   => 'Avanzar/Acercar',
 
    RESET    => 'Reiniciar',
+
+   FLIP     => 'Dar la vuelta',
 
    DOWNLOAD_FILE    => 'Bajar el Documento',
 
@@ -89,9 +155,19 @@ END
 
    IMAGE_WIDTH => 'Ancho de la Imagen',
 
-   BETWEEN     => 'Entre',
+   BETWEEN     => 'Entre Dos Puntos de Referencia',
 
    BENEATH     => 'Debajo',
+
+   LEFT        => 'Izquierda',
+
+   RIGHT       => 'Derecha',
+
+   TRACK_NAMES => 'Nombre de la Tabla de Pistas',
+
+   ALPHABETIC  => 'Alfabético',
+
+   VARYING     => 'Variando/Variante',
 
    SET_OPTIONS => 'Definir Opciones para Pistas/Trayectoria...',
 
@@ -136,6 +212,8 @@ END
 
    HIT_COUNT      => 'Las siguientes %d regiones coinciden con la que solicitaste.',
 
+   POSSIBLE_TRUNCATION  => 'Resultados de búsqueda están limitados a %d aciertos; la lista puede ser incompleta.',
+
    MATCHES_ON_REF => 'Cantidad de regiones que coinciden en %s',
 
    SEQUENCE        => 'secuencia',
@@ -167,7 +245,7 @@ END
    SETTINGS_INSTRUCTIONS => <<END,
 La casilla de <i>Mostrar</i> enciende y apaga la pista. La
 opción <i>Compactar</i> obliga a condensar la pista, de manera que
-las anotaciones se sobrepondrán. Las opciones <i>Expander</i> e^M <i>Hiperexpander</i> encienden el control de colisión usando algoritmos de diseño más lentos y^M más rápidos. Las opciones <i>Expander</i> &amp; <i>etiqueta</i> e <i>Hiperexpander &amp; etiqueta</i> obligan a las anotaciones a ser marcadas. Si
+las anotaciones se sobrepondrán. Las opciones <i>Expander</i> e^M <i>Hiperexpander</i> encienden el control de colisión usando algoritmos de diseño más lentos y^M más rápidos. Las opciones <i>Expander</i> &amp; <i>etiqueta</i> e <i>Hiper-extender &amp; etiqueta</i> obligan a las anotaciones a ser marcadas. Si
 <i>Auto</i> es seleccionado, el control de colisión y las opciones de etiquetado serán
 colocadas automáticamente si el espacio lo permite. Para cambiar el orden de las pistas usar
 el menú emergente <i>Cambiar el Orden de las Pistas</i> para asignar una anotación a una

2663b5f02d86796e5d40571ef24a57808dbaf153
Lincoln Stein <lincoln.stein@gmail.com>
2005-03-04 18:43:54 +0000

    Patched over problem with the memory adaptor.  Problem is fixed in bioperl CVS

--- lib/Bio/Graphics/Browser/Util.pm
+++ lib/Bio/Graphics/Browser/Util.pm
@@ -143,7 +143,7 @@ sub open_database {
   }
 
   ################################################
-  # HACK ALERT - REMOVE AFTER BIOPERL 2.0 RELEASED
+  # HACK ALERT
   patch_old_versions_of_bioperl($adaptor);
   ################################################
 
@@ -227,123 +227,24 @@ sub html_frag {
 
 sub patch_old_versions_of_bioperl {
   my $adaptor = shift;
+  my %argv    = @_;
   local $^W = 0;
-  if ($adaptor eq 'Bio::DB::GFF') {
-
-    # patch missing is_circular method
-    eval <<'END' unless defined &Bio::DB::GFF::Segment::is_circular;
-sub Bio::DB::GFF::Segment::is_circular { 0; }
-END
-
-    warn $@ if $@;
-
-    # patch problems on Windows platforms with memory adaptor
-    # (having to do with broken glob() in perl 5.8)
-  if ($^O =~ /^MSWin/ && $Bio::DB::GFF::VERSION <= 1.4) {
-
+  if ($adaptor eq 'Bio::DB::GFF' && $argv{-adaptor} eq 'memory' && $Bio::Perl::VERSION <= 1.5) {
+    # patch memory.pm inability to handle missing gclass fields
     eval <<'END';
-sub Bio::DB::GFF::setup_argv {
+use Bio::DB::GFF::Adaptor::memory;
+sub Bio::DB::GFF::Adaptor::memory::load_gff_line {
   my $self = shift;
-  my $file_or_directory = shift;
-  my @suffixes          = @_;
-  no strict 'refs';  # so that we can call fileno() on the argument
-  my @argv;
-
-  if (-d $file_or_directory) {
-    # Because glob() is broken with long file names that contain spaces
-    $file_or_directory = Win32::GetShortPathName($file_or_directory)
-      if $^O =~ /^MSWin/i && eval "use Win32; 1;";
-    @argv = map { glob("$file_or_directory/*.{$_,$_.gz,$_.Z,$_.bz2}")} @suffixes;
-  }elsif (my $fd = fileno($file_or_directory)) {
-    open STDIN,"<&=$fd" or $self->throw("Can't dup STDIN");
-    @argv = '-';
-  } elsif (ref $file_or_directory) {
-    @argv = @$file_or_directory;
-  } else {
-    @argv = $file_or_directory;
-  }
-
-  foreach (@argv) {
-    if (/\.gz$/) {
-      $_ = "gunzip -c $_ |";
-    } elsif (/\.Z$/) {
-      $_ = "uncompress -c $_ |";
-    } elsif (/\.bz2$/) {
-      $_ = "bunzip2 -c $_ |";
-    }
-  }
-  @argv;
-}
-END
-   warn $@ if $@;
-   eval <<'END';
-sub Bio::Graphics::Feature attributes {
-  return;
-}
-END
-;
-   warn $@ if $@;
-
-   eval <<'END';
-sub Bio::DB::Fasta::new {
-  my $class = shift;
-  my $path  = shift;
-  my %opts  = @_;
-
-  my $self = bless { debug      => $opts{-debug},
-		     makeid     => $opts{-makeid},
-		     glob       => $opts{-glob}    || '*.{fa,fasta,FA,FASTA,fast,FAST,dna,fsa}',
-		     maxopen    => $opts{-maxfh}   || 32,
-		     dbmargs    => $opts{-dbmargs} || undef,
-		     fhcache    => {},
-		     cacheseq   => {},
-		     curopen    => 0,
-		     openseq    => 1,
-		     dirname    => undef,
-		     offsets    => undef,
-		   }, $class;
-  my ($offsets,$dirname);
-
-  if (-d $path) {
-    # Because Win32 glob() is broken with respect to 
-    # long file names that contain spaces
-    $path = Win32::GetShortPathName($path)
-      if $^O =~ /^MSWin/i && eval 'use Win32; 1';
-    $offsets = $self->index_dir($path,$opts{-reindex});
-    $dirname = $path;
-  } elsif (-f _) {
-    $offsets = $self->index_file($path,$opts{-reindex});
-    $dirname = dirname($path);
-  } else {
-    $self->throw( "$path: Invalid file or dirname");
-  }
-  @{$self}{qw(dirname offsets)} = ($dirname,$offsets);
-
-  $self;
-}
-END
-    ;
-    warn $@ if $@;
-    }
-  }
-
-   # patch versions of Bio::Graphics::FeatureFile that don't have the "make_link"
-   # method
-   unless (Bio::Graphics::FeatureFile->can('make_link')) {
-   eval <<'END';
-sub Bio::Graphics::FeatureFile::make_link {
-  my $self             = shift;
-  my ($feature,$panel) = @_;
-
-  for my $label ($self->feature2label($feature)) {
-    my $linkrule     = $self->setting($label,'link');
-    $linkrule        = $self->setting(general=>'link') unless defined $linkrule;
-    return $self->link_pattern($linkrule,$feature,$panel);
-  }
+  my $feature_hash  = shift;
+  $feature_hash->{strand} = '' if $feature_hash->{strand} && $feature_hash->{strand} eq '.';
+  $feature_hash->{phase}  = ''  if $feature_hash->{phase}  && $feature_hash->{phase} eq '.';
+  $feature_hash->{gclass} = 'Sequence' if length $feature_hash->{gclass} == 0;
+  # sort by group please
+  push @{$self->{tmp}{$feature_hash->{gclass},$feature_hash->{gname}}},$feature_hash;
 }
 END
   warn $@ if $@;
-}
+  }
 }
 
 sub redirect_legacy_url {

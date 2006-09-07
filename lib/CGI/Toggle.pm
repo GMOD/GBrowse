87c602187913d4b6cf3b3a0b3637219c0d1873a1
Lincoln Stein <lincoln.stein@gmail.com>
2006-09-07 14:32:28 +0000

    expansion & contraction of categories in the tracks section are now sticky; expansion state will no longer be messed up when adding or removing categories; XHTML 1.0 now validates on W3C validator site

--- lib/CGI/Toggle.pm
+++ lib/CGI/Toggle.pm
@@ -3,26 +3,20 @@ package CGI::Toggle;
 use strict;
 use base 'Exporter';
 use CGI 'div','span','img','url';
-use CGI::Util;
-
-use vars '$next_id','$VECTOR','$RESET';
 
 our @EXPORT = ('toggle_section',
-	       'start_html',
-	       'end_html');
+	       'start_html');
 
 use constant PLUS    => 'plus.png';
 use constant MINUS   => 'minus.png';
 use constant JS      => 'toggle.js';
-use constant EXPIRES => CGI::Util::expires('+1M');
+use constant XCOOK   => 'x_cook.js';
 
 my $image_dir = '/gbrowse/images/buttons';
 my $js_dir    = '/gbrowse/js';
 
 my $cookie_name = __PACKAGE__;
 $cookie_name    =~ s/:/_/g;
-
-my $cookie_expires = EXPIRES;
 
 my $style = <<'END';
 .el_hidden  {display:none}
@@ -38,25 +32,14 @@ my $style = <<'END';
 .tctl      {  text-decoration:underline; }
 END
 
-my $noscript = <<'END';
-<style>
-.el_hidden { display:inline }
-.nojs      { display:none }
-</style>
-END
-
 sub start_html {
-  $next_id = 'T0000';
-  $VECTOR  = 0;
-
   my %args = @_ == 1 ? (-title=>shift) : @_;
-
-  $args{-noscript}     = $noscript;
-  $args{-onLoad}       = "startPage()";
-  $RESET               = $args{-reset_toggle};
 
   $image_dir           = $args{-gbrowse_images} if defined $args{-gbrowse_images};
   $js_dir              = $args{-gbrowse_js}     if defined $args{-gbrowse_js};
+
+  delete $args{-gbrowse_images};
+  delete $args{-gbrowse_js};
 
   # earlier versions of CGI.pm don't support multiple -style and -script args.
   if ($CGI::VERSION >= 3.05) {
@@ -72,6 +55,7 @@ sub start_html {
     }
 
     push @{$args{-script}},{src=>"$js_dir/".JS};
+    push @{$args{-script}},{src=>"$js_dir/".XCOOK};
   }
 
   my $result = CGI::start_html(%args);
@@ -85,41 +69,33 @@ sub start_html {
   return $result;
 }
 
-sub end_html {
- my @script_section = $CGI::Q->_script({code=>"xSetCookie('$cookie_name',$VECTOR,'$cookie_expires')"})
-   unless $RESET || $VECTOR==0;
- return @script_section,CGI::end_html;
-}
-
 # The weird playing around with class names is to accomodate the need to have
 # a default setting of visibility that can be overridden by a stored cookie.
 sub toggle_section {
   my %config = ref $_[0] eq 'HASH' ? %{shift()} : ();
-  my ($section_title,@section_body) = @_;
-
-  my $id = $next_id++;
-  if (!$config{override} && !$RESET && (my $cookie = CGI::cookie($cookie_name))) {
-    $config{on} = ($cookie & (1<<substr($id,1)||0)) != 0;
-  }
+  my ($name,$section_title,@section_body) = @_;
+  my $visible = $config{on};
 
   my $plus  = $config{plus_img}  || "$image_dir/".PLUS;
   my $minus = $config{minus_img} || "$image_dir/".MINUS;
 
-  my $show_ctl = div({-id=>"${id}_show",
-		       -class=>$config{on} ? "ctl_hidden" : "ctl_visible",
-		       -onClick=>"visibility('$id','on','$cookie_name','$cookie_expires')"},
-		      img({-src=>$plus}).'&nbsp;'.span({-class=>'tctl'},$section_title));
-  my $hide_ctl = div({-id=>"${id}_hide",
-		       -class=>$config{on} ? "ctl_visible" : "ctl_hidden",
-		       -onClick=>"visibility('$id','off','$cookie_name','$cookie_expires')"},
-		      img({-src=>$minus}).'&nbsp;'.span({-class=>'tctl'},$section_title));
-  my $content  = div({-id    => $id,
-		      -class => $config{on} ? 'el_visible' : 'el_hidden'},
+  my $show_ctl = div({-id=>"${name}_show",
+		       -class=>'ctl_hidden',
+		       -style=>$visible ? 'display:none' : 'display:inline',
+		       -onClick=>"visibility('$name',1)"
+                     },
+		     img({-src=>$plus,-alt=>'+'}).'&nbsp;'.span({-class=>'tctl'},$section_title));
+  my $hide_ctl = div({-id=>"${name}_hide",
+		       -class=>'ctl_visible',
+		       -style=>$visible ? 'display:inline' : 'display:none',
+		       -onClick=>"visibility('$name',0)"
+                     },
+		     img({-src=>$minus,-alt=>'-'}).'&nbsp;'.span({-class=>'tctl'},$section_title));
+  my $content  = div({-id    => $name,
+		      -style=>$visible ? 'display:inline' : 'display:none',
+		      -class => 'el_visible'},
 		     @section_body);
   my @result = $config{nodiv} ? ($show_ctl.$hide_ctl,$content) : div(($show_ctl.$hide_ctl,$content));
-  my $val = substr($id,1);
-  $VECTOR   |= (1  << $val)  if $config{on};
-  $VECTOR   &= ~(1 << $val)  if !$config{on};
   return wantarray ? @result : "@result";
 }
 

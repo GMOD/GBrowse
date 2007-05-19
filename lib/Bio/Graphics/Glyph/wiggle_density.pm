37e2900c7bc45fca5b4c47bac222d98af6147f48
Lincoln Stein <lincoln.stein@gmail.com>
2007-05-19 21:01:16 +0000

    added support files for Jim Kent-format wiggle files

---
+++ lib/Bio/Graphics/Glyph/wiggle_density.pm
@@ -0,0 +1,127 @@
+package Bio::Graphics::Glyph::wiggle_density;
+
+# $Id: wiggle_density.pm,v 1.1.2.1 2007-05-19 21:01:16 lstein Exp $
+
+use strict;
+use base 'Bio::Graphics::Glyph::box';
+use Bio::Graphics::Wiggle;
+
+sub min_score {
+  shift->option('min_score') || 0.0;
+}
+
+sub max_score {
+  shift->option('max_score') || 1.0;
+}
+
+sub draw {
+  my $self = shift;
+  my ($gd,$left,$top,$partno,$total_parts) = @_;
+  my $feature   = $self->feature;
+  my ($wigfile) = $feature->attributes('wigfile');
+  my ($wigoffset) = $feature->attributes('wigstart');
+  return $self->SUPER::draw(@_) unless $wigfile;
+  my $wig = Bio::Graphics::Wiggle->new($wigfile) or die;
+
+  my ($x1,$y1,$x2,$y2) = $self->bounds($left,$top);
+  my $chr              = $feature->seq_id;
+  my $start            = $feature->start;
+  my $end              = $feature->end;
+
+  # filler -- this will get erased by the real data when it comes
+  my $middle = ($y1+$y2)/2;
+  my $fgcolor = $self->fgcolor;
+  $gd->line($x1,$middle-3,$x1,$middle+3,$fgcolor);   # vertical span
+  $gd->line($x1,$middle,$x2,$middle,$fgcolor); # horizontal span
+  $gd->line($x2,$middle-3,$x2,$middle+3,$fgcolor);   # vertical span
+
+  # find all overlapping segments in the wig file
+  warn "\nsearching for $start->$end";
+  my $iterator = $wig->segment_iterator($chr,$start-1,$end);
+  my $idx = 0;
+  while (my $seg = $iterator->next_segment) {
+    warn "got ",$seg->start,"->",$seg->span;
+    $self->draw_segment($gd,$seg,$start,$end,$x1,$y1,$x2,$y2,$idx++);
+  }
+
+  $self->draw_label(@_)       if $self->option('label');
+  $self->draw_description(@_) if $self->option('description');
+}
+
+sub draw_segment {
+  my $self = shift;
+  my ($gd,$seg,$start,$end,$x1,$y1,$x2,$y2,$idx) = @_;
+  my $seg_start = $seg->start + 1;   # adjust for zero-based coordinates
+  my $seg_end   = $seg_start  + $seg->span - 1;
+  my $step      = $seg->step;
+
+  # clip, because wig files do no clipping
+  $seg_start = $start      if $seg_start < $start;
+  $seg_end   = $end        if $seg_end   > $end;
+
+  # figure out where we're going to start
+  my $scale  = $self->scale;  # pixels per base pair
+  my $pixels_per_step = $scale * $step;
+
+  # if the feature starts before the data starts, then we need to draw
+  # a line indicating missing data (this only happens if something went
+  # wrong upstream)
+  if ($seg_start > $start) {
+    my $terminus = $self->map_pt($seg_start);
+    $start = $seg_start;
+    $x1    = $terminus;
+  }
+  # if the data ends before the feature ends, then we need to draw
+  # a line indicating missing data (this only happens if something went
+  # wrong upstream)
+  if ($seg_end < $end) {
+    my $terminus = $self->map_pt($seg_end);
+    $end = $seg_end;
+    $x2    = $terminus;
+  }
+
+  return unless $start < $end;
+
+  # get data values across the area
+  my $span = $end - $start + 1;
+  my @data = $seg->values($start-1,$span);
+
+  # number of data points should be equal to length/step...
+  @data == $span/$step
+    or die "number of data points should be equal to length/step: span=$span, step=$step, datapoints = ",
+      scalar @data;
+
+  my $min_value = $self->min_score;
+  my $max_value = $self->max_score;
+
+  # allocate colors
+  my @rgb = $self->panel->rgb($self->bgcolor);
+  my %color_cache;
+  for (my $i = $start; $i <= $end ; $i += $step) {
+    my $data_point = shift @data;
+    $data_point    = $min_value if $min_value > $data_point;
+    $data_point    = $max_value if $max_value < $data_point;
+    my ($r,$g,$b)  = $self->calculate_color($data_point,\@rgb,$min_value,$max_value);
+    my $idx        = $color_cache{$r,$g,$b} ||= $self->panel->translate_color($r,$g,$b);
+    $self->filled_box($gd,$x1,$y1,$x1+$pixels_per_step,$y2,$idx,$idx);
+    $x1 += $pixels_per_step;
+  }
+
+}
+
+sub calculate_color {
+  my $self = shift;
+  my ($s,$rgb,$min_score,$max_score) = @_;
+  return map { 255 - (255-$_) * min(max( ($s-$min_score)/($max_score-$min_score), 0), 1) } @$rgb;
+}
+
+sub min { $_[0] < $_[1] ? $_[0] : $_[1] }
+sub max { $_[0] > $_[1] ? $_[0] : $_[1] }
+
+sub get_description {
+  my $self = shift;
+  my $feature = shift;
+  return join '',"wigFile = ",$feature->attributes('wigfile'),'; wig_offset=',$feature->attributes('wigstart');
+}
+
+1;
---
+++ lib/Bio/Graphics/Wiggle.pm
@@ -0,0 +1,249 @@
+package Bio::Graphics::Wiggle;
+
+=head1 NAME
+
+Bio::Graphics::Wiggle -- Binary storage for dense genomic features
+
+=head1 SYNOPSIS
+
+ my $wig = Bio::Graphics::Wiggle->new('./test.wig','writable') or die;
+
+ $offset = $wig->add_segment('chr1',0,100,1000),"\n";
+ $wig->add_values(1..10);
+
+ $offset = $wig->add_segment('chr1',2000,100,1000),"\n";
+ $wig->add_values([map {$_*10.1} (1..10)]);
+
+ $offset = $wig->add_segment('chr2',0,100,1000),"\n";
+ $wig->add_values(1..10);
+
+ undef $wig;
+ $wig = Bio::Graphics::Wiggle->new('./test.wig');
+
+ my $iterator = $wig->segment_iterator('chr1',0,500);
+ while (my $seg = $iterator->next_segment) {
+    print $seg->seqid," ",$seg->start,' ',$seg->step,' ',$seg->span,"\n";
+    print "\t",join ' ',$seg->values,"\n";
+ }
+
+ my $seg = $sig->segment_iterator('chr2')->next_segment;
+ my @values = $seg->values($seg->start,1000);  # fetch span = 1000 bp
+}
+
+=cut
+
+# read/write genome tiling data, to be compatible with Jim Kent's WIG format
+use strict;
+use warnings;
+use IO::File;
+
+use constant HEADER_LEN => 256;
+use constant HEADER => '(Z50LLLl)@'.HEADER_LEN; # seqid, start, step, span, next_block
+use constant BODY   => 'f';
+
+sub new {
+  my $self          = shift;
+  my ($path,$write) = @_;
+  my $mode = $write ? 'w+' : 'r';
+  my $fh = IO::File->new($path,$mode) or die "$path: $!";
+  if ($write) { $fh->seek(0,SEEK_END) };
+  return bless {fh          => $fh,
+		write       => $write,
+		last_offset => $fh->tell,
+	       }, ref $self || $self;
+}
+
+sub fh     { shift->{fh}    }
+sub seek   { shift->fh->seek(shift,0) }
+sub tell   { shift->fh->tell()        }
+
+# args
+# (seqid,start,end)
+# please use half-open intervals for start and end
+sub segment_iterator {
+  my $self = shift;
+  return Bio::Graphics::WiggleIterator->new($self,@_);
+}
+
+sub segment {
+  my $self = shift;
+  my $offset = shift;
+  Bio::Graphics::WiggleSegment->new($self,$offset);
+}
+
+sub find_segment {
+  my $self = shift;
+  my ($offset,$seqid,$start,$end) = @_;
+
+  warn "start = $start, end = $end";
+
+  $offset ||= 0;
+
+  $start ||= 0;
+  $end   ||= 999_999_999_999;
+
+  my $fh   = $self->fh;
+
+  my ($seqname,$strt,$step,$span,$next,$found);
+  while ($offset >= 0 && !defined $found) {
+    $fh->seek($offset,0)                                   or last;
+    ($seqname,$strt,$step,$span,$next) = $self->readheader or last;
+    next if defined $seqid && $seqid ne $seqname;
+    next unless $end > $strt;
+    next if $span && $start >= $strt+$span;
+    $found = $offset;
+  } continue {
+    $offset = $next;
+  }
+  return $found;
+}
+
+sub writable { shift->{write} }
+sub last_offset {
+  my $self = shift;
+  $self->{last_offset} = shift if @_;
+  $self->{last_offset};
+}
+
+sub add_segment {
+  my $self = shift;
+  my ($seqname,$start,$step,$span) = @_;
+  die "file not open for writing" unless $self->writable;
+  my $fh = $self->fh;
+  my $current = $fh->tell;
+
+  if ($current > 0) { # need to update the next pointer
+    $self->seek($self->last_offset,0);
+    my ($sn,$strt,$stp,$spn,$next) = $self->readheader();
+    $self->seek($self->last_offset,0);
+    $self->writeheader($sn,$strt,$stp,$spn,$current);
+    $self->seek($current);
+  }
+
+  $self->last_offset($current);
+  $self->writeheader($seqname,$start,$step,$span,-1);
+  return $current;
+}
+
+sub add_values {
+  my $self  = shift;
+
+  my $fh    = $self->fh;
+  for my $value (@_) {
+    my $data  = pack('f*',ref $value ? @$value : $value);
+    $fh->print($data) or die "write error: $!";
+  }
+
+}
+
+sub end_segment {
+  my $self = shift;
+  return $self->fh->tell;
+}
+
+sub readheader {
+  my $self = shift;
+  my $fh   = $self->fh;
+  my $header;
+  $fh->read($header,HEADER_LEN) == HEADER_LEN or die "read failed: $!";
+  return unpack(HEADER,$header);
+}
+
+sub writeheader {
+  my $self = shift;
+  my ($seqname,$start,$step,$span,$next) = @_;
+  my $fh = $self->fh;
+  my $header = pack(HEADER,$seqname,$start,$step,$span,$next);
+  $fh->print($header) or die "write failed: $!";
+}
+
+package Bio::Graphics::WiggleIterator;
+
+sub new {
+  my $self        = shift;
+  my $wiggle_file = shift;
+  my @search_args = @_;
+
+  return bless {
+		search_args => \@search_args,
+		wiggle_file => $wiggle_file,
+		offset      => 0,
+		},ref $self || $self;
+}
+
+sub next_segment {
+  my $self = shift;
+  my $offset = $self->{wiggle_file}->find_segment($self->{offset},@{$self->{search_args}});
+  return unless defined $offset;
+  my $segment = $self->{wiggle_file}->segment($offset) or return;
+  $self->{offset} = $segment->next;
+  $segment;
+}
+
+package Bio::Graphics::WiggleSegment;
+
+sub new {
+  my $self = shift;
+  my ($wig_file,$header_pos) = @_;
+  $wig_file->seek($header_pos);
+  my ($seqid,$start,$step,$span,$next) = $wig_file->readheader() or return;
+  return bless {
+		seqid   => $seqid,
+		start   => $start,
+		step    => $step,
+		span    => $span,
+		next    => $next,
+		wig     => $wig_file,
+		value_offset  => $header_pos + Bio::Graphics::Wiggle::HEADER_LEN,
+	       },ref $self || $self;
+}
+
+sub seqid  { shift->{seqid} }
+sub seq_id { shift->{seqid} }
+sub start { shift->{start} }
+sub step  { shift->{step}  }
+sub span  { shift->{span}  }
+sub next  { shift->{next}  }
+sub wig   { shift->{wig}   }
+sub value_offset { shift->{value_offset} }
+
+sub values {
+  my $self = shift;
+  my ($start,$span) = @_;
+
+  $start ||= $self->start;
+  $span  ||= $self->span - $start;
+
+  my $step        = $self->step;
+  my $block_start = int (($start - $self->start)/$step);
+
+  my $read_start  = $self->value_offset + $block_start * 4;
+  my $read_length = $span/$step * 4;
+  my $data;
+  $self->wig->seek($read_start);
+  $self->wig->fh->read($data,$read_length) or die "read error: $!";
+  return unpack('f*',$data);
+}
+
+1;
+
+__END__
+
+=head1 SEE ALSO
+
+L<Bio::Graphics::Panel>,
+L<Bio::Graphics::Glyph>,
+L<Bio::Graphics::Feature>,
+L<Bio::Graphics::FeatureFile>
+
+=head1 AUTHOR
+
+Lincoln Stein E<lt>lstein@cshl.orgE<gt>.
+
+Copyright (c) 2007 Cold Spring Harbor Laboratory
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.  See DISCLAIMER.txt for
+disclaimers of warranty.
+
+=cut

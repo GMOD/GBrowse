3ccafa13048fb8b6bb8c3dc328883741dd713b8e
Sheldon McKay <sheldon_mckay@users.sourceforge.net>
2008-03-20 00:26:34 +0000

    (un)checking the 'show tooltips' box submits the form so the page state matches the checkbox state; added a bit of formatting to the search instructions

--- conf/languages/en.pm
+++ conf/languages/en.pm
@@ -10,13 +10,14 @@
    PAGE_TITLE => 'Genome browser',
 
    SEARCH_INSTRUCTIONS => <<END,
-Search using a sequence name, gene name,
+<b>Searching:</b> Search using a sequence name, gene name,
 locus%s, or other landmark. The wildcard
 character * is allowed.
 END
 
    NAVIGATION_INSTRUCTIONS => <<END,
-Click one of the rulers to center on a location, or click and drag to
+<br>
+<b>Navigation:</b> Click one of the rulers to center on a location, or click and drag to
 select a region. Use the Scroll/Zoom buttons to change magnification
 and position.
 END

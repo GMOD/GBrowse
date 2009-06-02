39673992834d6b02eb79954420a7636af44119dc
Lincoln Stein <lincoln.stein@gmail.com>
2009-06-02 15:17:33 +0000

    this is the beginnings of an administrative interface for managing tracks

---
+++ lib/Bio/Graphics/Browser/Admin.pm
@@ -0,0 +1,26 @@
+package Bio::Graphics::Browser::Admin;
+
+#$Id: Admin.pm,v 1.1 2009-06-02 15:17:33 lstein Exp $
+#Remote administrative interface for GBrowse instances
+
+=head1 NAME
+
+Bio::Graphics::Browser::Admin -- Administrative interface for GBrowse
+
+=head1 SYNOPSIS
+
+my $adm = Bio::Graphics::Browser::Admin->new(
+                                             -source => 'yeast',
+                                             -
+                                             );
+
+=head1 DESCRIPTION
+
+=head2 METHODS
+
+=head1 AUTHOR
+
+Lincoln D. Stein <lincoln.stein@gmail.com>
+
+
+1;

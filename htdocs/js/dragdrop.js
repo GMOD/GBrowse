bb536b8878e30d0d84b6126e013af8ba5e8c61c2
Lincoln Stein <lincoln.stein@gmail.com>
2010-06-02 11:34:19 +0000

    adding potential workarounds for broken dragdrop on IE 7

--- htdocs/js/dragdrop.js
+++ htdocs/js/dragdrop.js
@@ -407,7 +407,9 @@ var Draggable = Class.create({
     if(this.options.ghosting) {
       if (!this.element._originallyAbsolute)
         Position.relativize(this.element);
-      delete this.element._originallyAbsolute;
+      // http://dev.rubyonrails.org/attachment/ticket/10207/dragdrop_originallyabsolute.diff
+      //      delete this.element._originallyAbsolute;
+      this.element._originallyAbsolute = null;
       Element.remove(this._clone);
       this._clone = null;
     }

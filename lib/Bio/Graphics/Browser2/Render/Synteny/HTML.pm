85711f11f452b0a4a314ea457fae650b1423e9df
Robert Buels <rmb32@cornell.edu>
2010-12-07 16:22:58 -0800

    get rid of @hits global and store it in the renderer obj
    Conflicts:
    
    	lib/Bio/Graphics/Browser2/Render/Synteny/HTML.pm

--- lib/Bio/Graphics/Browser2/Render/Synteny/HTML.pm
+++ lib/Bio/Graphics/Browser2/Render/Synteny/HTML.pm
@@ -61,7 +61,6 @@ our $SCONF;
 our $CONF;
 our $MAP;
 our $SYNTENY_IO;
-our @hits;
 
 sub new {
     my $class   = shift;
@@ -144,12 +143,15 @@ END
     }
 
     if ($segment) {
-
-      @hits = map {$SYNTENY_IO->get_synteny_by_range(-src   => $page_settings->{search_src},
-    						 -ref   => $segment->abs_ref,
-    						 -start => $segment->abs_start,
-    						 -end   => $segment->abs_end,
-    						 -tgt   => $_) } @requested_species;
+        $self->hits([
+            map {$SYNTENY_IO->get_synteny_by_range(
+                -src   => $page_settings->{search_src},
+                -ref   => $segment->abs_ref,
+                -start => $segment->abs_start,
+                -end   => $segment->abs_end,
+                -tgt   => $_)
+             } @requested_species
+        ]);
     }
 
     my $header = $CONF->setting('header');
@@ -165,23 +167,22 @@ END
     print overview_panel($CONF->whole_segment($segment),$segment) if $segment;
 
     if ($segment) {
-      # make sure no hits go off-screen
-      remap_coordinates($_) for @hits;
+        # make sure no hits go off-screen
+        remap_coordinates($_) for @{ $self->hits };
 
-      segment_info($page_settings,$segment);
+        segment_info($page_settings,$segment);
 
-      my %species = map {$_->src2 => 1} @hits;
-      my @species = sort keys %species;
+        my %species = map {$_->src2 => 1} @{ $self->hits };
+        my @species = sort keys %species;
 
-      # either display ref species + 2 repeating or 'all in one'
-      if ($page_settings->{display} eq 'expanded') {
-        while (my @pair = splice @species, 0, 2) {
-          draw_image($page_settings,\@hits,@pair);
+        # either display ref species + 2 repeating or 'all in one'
+        if ($page_settings->{display} eq 'expanded') {
+            while (my @pair = splice @species, 0, 2) {
+                draw_image($page_settings, $self->hits, @pair);
+            }
+        } else {
+            draw_image( $page_settings, $self->hits, @species );
         }
-      }
-      else {
-        draw_image($page_settings,\@hits,@species);
-      }
     }
 
     options_table();
@@ -1723,6 +1724,18 @@ sub hide {
   print hidden( -name     => $name,
                 -value    => $value,
                 -override => 1 ), "\n";
+}
+
+
+########## accessors ###########
+
+sub hits {
+    my $self = shift;
+    if( @_ ) {
+        my $h = $self->{hits} = shift;
+        croak 'hits must be an arrayref' unless ref $h eq 'ARRAY';
+    }
+    return $self->{hits};
 }
 
 

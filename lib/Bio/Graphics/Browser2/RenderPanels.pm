5c9c3f640478c360dd6a68f8eb7e153878b87879
Sonu Lall <lallsonu@users.sourceforge.net>
2011-02-03 22:35:35 +0000

    menu looks nice, and is completely functional--still working on getting expand to change to collapse onmousedown

--- lib/Bio/Graphics/Browser2/RenderPanels.pm
+++ lib/Bio/Graphics/Browser2/RenderPanels.pm
@@ -560,7 +560,7 @@ sub wrap_rendered_track {
         )
 
 	);
-   my $collapse_ipad = 'Collapse';
+   my $ipad_collapse = $collapsed ? 'Expand':'Collapse';
   my $cancel_ipad = 'Delete';
   my $share_ipad = 'Share';
   my $configure_ipad = 'Configure';
@@ -579,7 +579,7 @@ sub wrap_rendered_track {
  		-onClick =>  "collapse('$label')",
 		
 		},
- 		 div({-class => 'linkbg',},$collapse_ipad)),
+ 		 div({-class => 'linkbg',},$ipad_collapse)),
  	    div({-class => 'ipadcollapsed',
  		 -id => "${label}_kill",
  		 -onClick     => "ShowHideTrack('$label',false)",
@@ -608,12 +608,12 @@ sub wrap_rendered_track {
 		  
 		 
 
- 	    $self->if_not_ipad(@images,),
-	$self->if_ipad( span({-class => 'menuclick', -onClick=> "GBox.showTooltip(event,'load:popmenu_${label}')"}, 'Menu |')),		
+#  	    $self->if_not_ipad(@images,),
+	span({-class => 'menuclick', -onClick=> "GBox.showTooltip(event,'load:popmenu_${label}')"}, 'Menu |'),		
 	 
-#  	   
+#  	   $self->if_ipad( )
 	   
-	    span({-class => 'drag_region',},$title)
+	    span({-class => 'drag_region',},$title),
 
 	);
 

bf34a94a07b635bc42accc60429e688de55e77fd
Dan Bolser <dan.bolser@gmail.com>
2011-08-08 17:14:47 +0100

    rewrote gbrowse_syn_load_alignment_database.pl to use IDENTICAL copies of the subroutines used in gbrowse_syn_load_alignments_msa.pl. The next step is to split these subs out, or merge the two scripts.

--- bin/gbrowse_syn_load_alignment_database.pl
+++ bin/gbrowse_syn_load_alignment_database.pl
@@ -1,70 +1,95 @@
 #!/usr/bin/perl -w 
-# $Id: load_alignment_database.pl,v 1.1.2.4 2009-06-03 08:09:26 sheldon_mckay Exp $
+
 use strict;
 
 # load_alignment_database.pl  -- a script to load the database for gbrowse_syn.
 
 # The expected file format is tab-delimited (shown below):
-# species1  ref1  start1 end1 strand1  cigar_string1 species2  ref2  start2 end2 strand2  cigar_string2 coords1... | coords2...
-# the coordinate format: pos1_species1 pos1_species2 ... posn_species1 posn_species2 | pos1_species2 pos1_species1 ... posn_species2 posn_species1, 
-# where pos is the matching sequence coordinate (ungapped) in each species.
-#
+# species1  ref1  start1 end1 strand1  cigar_string1 \
+# species2  ref2  start2 end2 strand2  cigar_string2 \
+# coords1... | coords2...
+
+# the coords (coordinate) format:
+# pos1_species1 pos1_species2 ... posn_species1 posn_species2 | \
+# pos1_species2 pos1_species1 ... posn_species2 posn_species1
+
+# where pos is the matching sequence coordinate (ungapped) in each
+# species.
+
 
 use strict;
+
+use Getopt::Long;
 use DBI;
 use Bio::DB::GFF::Util::Binning 'bin';
-use Getopt::Long;
 
 use constant MINBIN   => 1000;
 
-use vars qw/$create $user $pass $dsn $verbose $create $aln_idx/;
+use constant VERBOSE  => 0;
+use constant MAPRES   => 100;
 
-$| = 1;
+use vars qw/$format $create $user $pass $dsn $aln_idx $verbose $nomap $mapres $hit_idx $pidx %map/;
+
 GetOptions(
+	   'format=s'    => \$format,
            'user=s'      => \$user,
-           'pass=s'      => \$pass,
-           'dsn=s'       => \$dsn,
-           'verbose'     => \$verbose,
-           'create'      => \$create
-           );
+	   'pass=s'      => \$pass,
+	   'dsn=s'       => \$dsn,
+           'map=i'       => \$mapres,
+	   'verbose'     => \$verbose,
+	   'nomap'       => \$nomap,
+	   'create'      => \$create
+	   );
 
-my $usage = "Usage: load_alignment_database.pl -u username -p password -d database [-v, -c] file1, file2 ... filen\n\n";
+my $usage = "Usage: load_alignment_database.pl -u username -p password -d database [-m map_resolution, -v, -n, -c] file1, file2 ... filen\n\n";
 
-$dsn     || die "Error: no database name\n$usage";
-$user    || die "Error: no user name\n$usage";
+$dsn     || die $usage;
+$user    || die $usage;
 
-if ($create && $verbose) {
-  print STDERR "\nNote: a new database $dsn will be initialized\n EXISTING DATA WILL BE DELETED\n\n";
+$verbose ||= VERBOSE;
+$mapres  ||= MAPRES;
+
+my ($dbh,$sth_hit,$sth_map) = prepare_database($dsn,$user,$pass);
+
+while (<>) {
+  chomp;
+  my ($src1,$ref1,$start1,$end1,$strand1,$seq1,
+      $src2,$ref2,$start2,$end2,$strand2,$seq2,@maps) = split "\t";
+
+  # deal with coordinate maps
+  my ($switch,@map1,@map2);
+  for (@maps) {
+    if ($_ eq '|') {
+      $switch++;
+      next;
+    }
+    $switch ? push @map2, $_ : push @map1, $_;    
+  }
+  my %map1 = @map1;
+  my %map2 = @map2;
+  
+  load_alignment($src1,$ref1,$start1,$end1,$strand1,$seq1,
+                 $src2,$ref2,$start2,$end2,$strand2,$seq2,\%map1,\%map2);
 }
-elsif ($verbose) {
-  print STDERR "\nNote: data will be appended to the existing database $dsn\n\n";
-}
+
+done();
 
 
-$dsn = "dbi:mysql:$dsn" unless $dsn =~ /^dbi/;
-my $dbh = DBI->connect($dsn, $user, $pass);
-unless ($dbh) {
-  $dsn =~ s/\S+\:([^:]+)$/$1/;
-  my $error = DBI->errstr;
-  die <<END;
 
- Error: $error
 
- There was a problem opening a connection to database $dsn.
- Make sure you have used the correct username ($user), password ($pass) and that you have 
- created the database $dsn via your mysql client. 
- 
- Consult the error message above for more information.
-END
-;
-}
 
-if ($create) {
-  $dbh->do('drop table if exists alignments') or die DBI->errstr;
-  $dbh->do('drop table if exists map') or die DBI->errstr;
+sub prepare_database {
+  my ($dns,$user,$pass) = @_;
+  $dsn = "dbi:mysql:$dsn" unless $dsn =~ /^dbi/;
 
-  $dbh->do(<<END) or die DBI->errstr;
-  create table alignments (
+  my $dbh = DBI->connect($dsn, $user, $pass) or die DBI->errstr;
+
+  if ($create) {
+    $dbh->do('drop table if exists alignments') or die DBI->errstr;
+    $dbh->do('drop table if exists map') or die DBI->errstr;
+
+    $dbh->do(<<"    END;") or die DBI->errstr;
+    create table alignments (
 			 hit_id    serial not null,
 			 hit_name  varchar(100) not null,
 			 src1      varchar(100) not null,
@@ -85,101 +110,90 @@ if ($create) {
 
     alter table alignments add constraint strand1_val CHECK( strand1='+' OR strand1 ='-' );
     alter table alignments add constraint strand2_val CHECK( strand2='+' OR strand2 ='-' );
-END
-;
+    END;
 
-  $dbh->do(<<END) or die DBI->errstr;
-  create table map (
-		  map_id serial not null,
+    $dbh->do(<<"    END;") or die DBI->errstr;
+    create table map (
+	  	  map_id serial not null,
 		  hit_name  varchar(100) not null,
                   src1 varchar(100),
 		  pos1 int not null,
 		  pos2 int not null,
 		  primary key(map_id)
 		  );
+    create index map_hit_name on map (hit_name);
+    END;
+  }
 
-  create index map_hit_name on map (hit_name);
-END
-;
-}
-
-my $sth=$dbh->prepare(<<END) or die $dbh->errstr;
-insert into alignments (hit_name,src1,ref1,start1,end1,strand1,seq1,bin,src2,ref2,start2,end2,strand2,seq2) 
+  my $hit_insert = $dbh->prepare(<<END) or die $dbh->errstr;
+insert into alignments (hit_name,src1,ref1,start1,end1,strand1,seq1,bin,src2,ref2,start2,end2,strand2,seq2)
 values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)
 END
 ;
 
-my $sth2 = $dbh->prepare('insert into map (hit_name,src1,pos1,pos2) values(?,?,?,?)');
+  my $map_insert = $dbh->prepare('insert into map (hit_name,src1,pos1,pos2) values(?,?,?,?)');
 
-my $hit_idx;
-while (<>) {
-  chomp;
-  my ($src1,$ref1,$start1,$end1,$strand1,$seq1,$src2,$ref2,$start2,$end2,$strand2,$seq2,@maps) = split "\t";
+  return $dbh, $hit_insert, $map_insert;
+}
+
+sub load_alignment {
+  my ($src1,$ref1,$start1,$end1,$strand1,$seq1,
+      $src2,$ref2,$start2,$end2,$strand2,$seq2,$map1,$map2) = @_;
 
   # not using the cigar strings right now
-   ($seq1,$seq2) = ('','');  
+  ($seq1,$seq2) = ('','');  
 
-  # deal with coordinate maps
-  my ($switch,@map1,@map2);
-  for (@maps) {
-    if ($_ eq '|') {
-      $switch++;
-      next;
-    }
-    $switch ? push @map2, $_ : push @map1, $_;    
-  }
-  my %map1 = @map1;
-  my %map2 = @map2;
+  $map1 ||= {};
+  $map2 ||= {};
+
+  my %map1 = %$map1;
+  my %map2 = %$map2;
 
   # standardize hit names
-  my $hit1 = 'H'.pad(++$hit_idx);
+  my $hit1 = 'H' . sprintf('%010s', ++$hit_idx);
   my $bin1 = scalar bin($start1,$end1,MINBIN);
   my $bin2 = scalar bin($start2,$end2,MINBIN);
   my $hit2 = "${hit1}r";
-  
+
   # force ref strand to always be positive and invert target strand as required
-  invert(\$strand1,\$strand2) if $strand1 eq '-'; 
+  invert(\$strand1,\$strand2) if $strand1 eq '-';
 
-  $sth->execute($hit1,$src1,$ref1,$start1,$end1,$strand1,$seq1,$bin1,
-		$src2,$ref2,$start2,$end2,$strand2,$seq2) or warn $sth->errstr;
+  $sth_hit->execute($hit1,$src1,$ref1,$start1,$end1,$strand1,$seq1,$bin1,
+		    $src2,$ref2,$start2,$end2,$strand2,$seq2) or warn $sth_hit->errstr;
 
-  for my $pos (sort {$a<=>$b} keys %map1) {
-    next unless $pos && $map1{$pos};
-    $sth2->execute($hit1,$src1,$pos,$map1{$pos}) or die $sth->errstr; 
+  unless ($nomap) {
+    for my $pos (sort {$a<=>$b} keys %map1) {
+      next unless $pos && $map1{$pos};
+      $sth_map->execute($hit1,$src1,$pos,$map1{$pos}) or die $sth_map->errstr; 
+    }
   }
 
   # reciprocal hit is also saved to facilitate switching amongst reference sequences
   invert(\$strand1,\$strand2) if $strand2 eq '-';
-  
-  $sth->execute($hit2,$src2,$ref2,$start2,$end2,$strand2,$seq2,$bin2,
-		$src1,$ref1,$start1,$end1,$strand1,$seq1) or warn $sth->errstr;
+
+  $sth_hit->execute($hit2,$src2,$ref2,$start2,$end2,$strand2,$seq2,$bin2,
+		    $src1,$ref1,$start1,$end1,$strand1,$seq1) or warn $sth_hit->errstr;
 
 
   # saving pair-wise coordinate maps -- these are needed for gridlines
-  for my $pos (sort {$a<=>$b} keys %map2) {
-    next unless $pos && $map2{$pos};
-    $sth2->execute($hit1,$src2,$pos,$map2{$pos}) or die $sth->errstr;
+  unless ($nomap) {
+    for my $pos (sort {$a<=>$b} keys %map2) {
+      next unless $pos && $map2{$pos};
+      $sth_map->execute($hit1,$src2,$pos,$map2{$pos}) or die $sth_map->errstr;
+    }
   }
 
-  print STDERR " processed $hit1!\r" if $verbose;
+  print STDERR "Processed pair-wise alignment ".++$pidx."\r" if $verbose;
 }
 
-$dbh->do('create index alignments_index1 on alignments (src1,ref1,bin,start1,end1);alter table alignments enable keys');
-
-print "\nDone\n";
-
-sub pad {
-  my $num = shift;
-  until ((length $num) >=6) {
-    $num = '0' . $num;
-  }
-  $num;
+sub done {
+  $dbh->do('create index alignments_index1 on alignments (src1,ref1,bin,start1,end1);alter table alignments enable keys');
+  print "\nDone!\n\n";
 }
-
 
 sub invert {
   my $strand1 = shift;
   my $strand2 = shift;
   $$strand1 = $$strand1 eq '+' ? '-' : '+';
-  $$strand2 = $$strand2 eq '+' ? '-' : '+'; 
+  $$strand2 = $$strand2 eq '+' ? '-' : '+';
 }

54e32dcb4950d8abed45da74bb98beb01de0ebe2
Lincoln Stein <lincoln.stein@gmail.com>
2012-09-06 01:19:40 -0400

    Volumes added to cloud instance have deleteOnTermination set to true.

--- bin/gbrowse_grow_cloud_vol.pl
+++ bin/gbrowse_grow_cloud_vol.pl
@@ -92,6 +92,7 @@ $vol->add_tag(Name=>"GBrowse lvm disk $local_device");
 print STDERR "Attaching volume.\n";
 my $a = $vol->attach($instance=>$ebs_device)                     or die "Couldn't attach EBS volume to $ebs_device: ",$ec2->error_str;
 $ec2->wait_for_attachments($a);
+$a->deleteOnTermination(1);
 -e $local_device                                                 or die "EBS volume did not appear at $local_device as expected";
 
 print STDERR "Creating lvm physical device.\n";

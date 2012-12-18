e8d73acb7a071413048064e5317d6f45114e7aad
Lincoln Stein <lincoln.stein@gmail.com>
2012-12-18 15:59:04 -0500

    one more try to hide command-line args

--- bin/gbrowse_launch_aws_slaves.pl
+++ bin/gbrowse_launch_aws_slaves.pl
@@ -11,12 +11,6 @@ my $balancer;
 $SIG{TERM} = sub {exit 0};
 $SIG{INT}  = sub {exit 0};
 
-# remove EC2 secrets from command line
-# (not 100% safe)
-foreach (@ARGV) {
-    s/(--?[as][^=\s]+(?:=|\s+)?)\S+/$1xxxxxxxxxx/;
-}
-
 my($ConfFile,$AccessKey,$SecretKey,$PidFile,$LogFile,$Daemon,$User,$Verbosity,$Kill);
 GetOptions(
 	   'access_key=s'  => \$AccessKey,
@@ -30,6 +24,13 @@ GetOptions(
 	   'background'    => \$Daemon,
 
     ) or exec 'perldoc',$0;
+
+# remove EC2 secrets from command line
+# (not 100% safe)
+foreach (@ARGV) {
+    s/(--?[as][^=\s]+(?:=|\s+)?)\S+/$1xxxxxxxxxx/;
+}
+$0 = "$0 @ARGV";
 
 $ConfFile  ||= File::Spec->catfile(GBrowse::ConfigData->config('conf'),'aws_slave.conf');
 

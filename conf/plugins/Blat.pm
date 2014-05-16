3cb38dbcd5c7acaeb981f3e52bbb4a2cf40be468
Nathan Weeks <weeks@iastate.edu>
2014-05-16 11:51:49 -0500

    BLAT hits are not necessarily reported in best-to-worst order. Read all blat hits and sort by calculated score (percent query coverage).
    Add 1 to start coordinates, as Start coordinates in BLAT PSL output are
    0-based, not 1-based.
    
    Strand needs to be +1/-1 (rather than "+" or "-") in order for
    Bio::SeqFeature::Lite (base class of Bio::Graphics::Feature) to
    correctly interpret them (though there is a pull request to fix this:
        https://github.com/bioperl/bioperl-live/pull/71

--- conf/plugins/Blat.pm
+++ conf/plugins/Blat.pm
@@ -121,35 +121,69 @@ sub find {
 
   open (IN, "$out_file") || die "couldn't open $out_file $!\n";
   my $hit_count = 0;
+  my @blat_hits;
+  # indexes each blat hit array of @blat_hits
+  use constant {
+    MATCHES       => 0,
+    MISMATCHES    => 1,
+    REP_MATCHES   => 2,
+    N_COUNT       => 3,
+    Q_NUM_INSERT  => 4,
+    Q_BASE_INSERT => 5,
+    T_NUM_INSERT  => 6,
+    T_BASE_INSERT => 7,
+    STRAND        => 8,
+    Q_NAME        => 9,
+    Q_LENGTH      => 10,
+    Q_START       => 11,
+    Q_END         => 12,
+    T_NAME        => 13,
+    T_LENGTH      => 14,
+    T_START       => 15,
+    T_END         => 16,
+    BLOCK_COUNT   => 17,
+    BLOCK_SIZES   => 18,
+    Q_STARTS      => 19,
+    T_STARTS      => 20,
+  };
+
   while(<IN>) {
+    push (@blat_hits, [ split ]);
+  }
+
+  for my $hit (
+    # sort hits in descending order by calculated score
+    sort {
+      ($b->[MATCHES]+$b->[MISMATCHES]+$b->[REP_MATCHES])/$b->[Q_LENGTH]
+      <=>
+      ($a->[MATCHES]+$a->[MISMATCHES]+$a->[REP_MATCHES])/$a->[Q_LENGTH]
+    } @blat_hits
+  ) {
     last if ++$hit_count > $hits;
-    # this could probably be done better but ...
-    my ( $matches,$mismatches,$rep_matches,$n_count,$q_num_insert,$q_base_insert,$t_num_insert, $t_base_insert,
-         $strand,$q_name,$q_length,$q_start,$q_end,$t_name,$t_length,$t_start,$t_end,$block_count,$block_sizes,
-         $q_starts,$t_starts) = split;
     
-    $block_sizes =~ s/\,$//;	      # remove trailing comma from block_sizes string     
-    $t_starts	 =~ s/\,$//;	      # .. and from t_starts string
-    my $score = sprintf "%.2f", ( 100 * ( $matches + $mismatches + $rep_matches ) / $q_length );
-    my $percent_id = sprintf "%.2f", ( 100 * ($matches + $rep_matches)/( $matches + $mismatches + $rep_matches ));
-    my $alignment = Bio::Graphics::Feature->new(-start=>$t_start,
-						-end  =>$t_end,
-						-ref => $t_name,
+    $hit->[BLOCK_SIZES] =~ s/\,$//;      # remove trailing comma from block_sizes string     
+    $hit->[T_STARTS]    =~ s/\,$//;      # .. and from t_starts string
+    my $score = sprintf "%.2f", ( 100 * ( $hit->[MATCHES] + $hit->[MISMATCHES] + $hit->[REP_MATCHES] ) / $hit->[Q_LENGTH] );
+    my $percent_id = sprintf "%.2f", ( 100 * ($hit->[MATCHES] + $hit->[REP_MATCHES]) /
+                                             ($hit->[MATCHES] + $hit->[MISMATCHES] + $hit->[REP_MATCHES]));
+    my $alignment = Bio::Graphics::Feature->new(-start=>$hit->[T_START]+1,
+						-end  =>$hit->[T_END],
+						-ref => $hit->[T_NAME],
 						-type=>'BLAT',
 						-name => "Alignment$hit_count",
-						-strand => $strand,
+						-strand => ($hit->[STRAND] eq '+') ? 1 : -1,
 						-score => $score
 					       );
     
-    @hit_starts = split(",", $t_starts);
-    @block_sizes = split(",", $block_sizes);
-    for($i=0;$i<$block_count;$i++){	# if multihit alignments (block_count > 1), aggregate.
+    @hit_starts = map { $_ + 1 } split(",", $hit->[T_STARTS]);
+    @block_sizes = split(",", $hit->[BLOCK_SIZES]);
+    for($i=0;$i<$hit->[BLOCK_COUNT];$i++){      # if multihit alignments (block_count > 1), aggregate.
       my $sub_alignment = Bio::Graphics::Feature->new(-start=>$hit_starts[$i],
         				      -end  =>($hit_starts[$i]+$block_sizes[$i]),
-        				      -ref => $t_name,
+        				      -ref => $hit->[T_NAME],
         				      -type=>'BLAT',
         				      -name => 'Alignment',
-        				      -strand => $strand,
+        				      -strand => ($hit->[STRAND] eq '+') ? 1 : -1,
         				      -score => $percent_id
         				     );
       $alignment->add_segment($sub_alignment);

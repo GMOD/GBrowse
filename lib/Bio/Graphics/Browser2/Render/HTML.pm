460baf150d268c85c004b532f0998434b3e12347
Nathan T. Weeks <nathan.weeks@ars.usda.gov>
2015-03-04 08:08:19 -0600

    Left-align citation text in "?" menu.

--- lib/Bio/Graphics/Browser2/Render/HTML.pm
+++ lib/Bio/Graphics/Browser2/Render/HTML.pm
@@ -2227,7 +2227,7 @@ sub track_citation {
     my $id       = $self->tr('TRACK_ID',$label);
     my $download = a({-href=>"?l=$label;f=save+datafile"},$self->tr('DOWNLOAD_TRACK_DATA_ALL'));
     my $title    = div({-style => 'background:gainsboro;padding:5px;font-weight:bold'},$key,br(),$id,br(),$download);
-    return  join '',div({-style=>'text-align:center;font-size:small'},$title,$cit_html);
+    return  join '',div({-style=>'text-align:left;font-size:small'},$title,$cit_html);
 }
 
 sub download_track_menu {
